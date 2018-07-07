f6c15047782bcede9fbf94aa47ef0633613c3625
Onion－Shen <18513385631@163.com>
2018-07-07 16:50:44 +0800

    delete JSONToken.*pp

--- src/JSON/JSONLexer.cpp
+++ src/JSON/JSONLexer.cpp
@@ -1,3 +1,5 @@
+#pragma mark -- JSONLexer
+
 #include "JSONLexer.hpp"
 #include <cctype>
 #include <regex>
@@ -351,4 +353,31 @@ JSONToken * JSONLexer::nullState(int16_t ch)
     }
 
     return new JSONToken(TokenType::Null, std::move(nullStr));
+}
+
+#pragma mark -- JSONToken
+JSONToken::JSONToken(TokenType _type, std::string _content)
+{
+    type = _type;
+    content = _content;
+}
+
+bool JSONToken::isElementType()
+{
+    return type == TokenType::Null || type == TokenType::String || isNumberType();
+}
+
+bool JSONToken::isContainer()
+{
+    return type == TokenType::LeftBracket || type == TokenType::LeftBrace;
+}
+
+bool JSONToken::isNumberType()
+{
+    return type == TokenType::Integer || type == TokenType::Float || type == TokenType::Boolean;
+}
+
+bool JSONToken::isValueType()
+{
+    return isContainer() || isElementType();
 }
